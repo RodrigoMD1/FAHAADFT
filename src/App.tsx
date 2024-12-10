7240af58693340f6348ab0d79cc6385094628562
RodrigoDuvivier <rodrigo.martinez224@gmail.com>
2024-12-10 17:55:24 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+    <div>
+      holaaa
+    </div>
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+
+const routes = [
+  {
+    path: "/",
+    element: <App />
+  }
+]
+
+
+const router = createBrowserRouter(routes)
+
+
+createRoot(document.getElementById('root')!).render(
+  
+  <StrictMode>
+    
+    <RouterProvider router={router} />
+
+  </StrictMode>,
+)
